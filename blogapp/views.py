2d50ffdf931ad247839985f2698c7fc71952e1b7
Sea <wantedsaea@gmail.com>
2019-06-29 13:43:24 +0900

    sortby id로바꿔봄

--- blogapp/views.py
+++ blogapp/views.py
@@ -38,7 +38,7 @@ def index(request) :
             return render(request, 'index.html', {'error': 'username or password is incorrect.'})
     else:
         blog = Blog.objects
-        blog_list = Blog.objects.get_queryset().order_by('title')
+        blog_list = Blog.objects.get_queryset().order_by('id')
         paginator = Paginator(blog_list, 3)
         page = request.GET.get('page')
         posts = paginator.get_page(page)
